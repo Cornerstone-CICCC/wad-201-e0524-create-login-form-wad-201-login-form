79cf574d2632950996824e65ecf0ffa7f0e476c1
Kevin Patino <kevinpatcastrejon95@gmail.com>
2024-07-10 16:54:41 -0700

    I just need to fix the border color & add password visibility

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const emailForm = document.getElementById("email");
 const password = document.getElementById("password");
-
+const checkbox = document.getElementById("checkbox");
+const loginButton = document.getElementById("login-but");
 
 emailForm.addEventListener("click", () => {
     emailForm.classList.add("focus-email");
-    password.classList.remove("focus-pass");
-})
+});
+
+emailForm.addEventListener("blur", () => {
+    emailForm.classList.remove("focus-email");
+});
 
 password.addEventListener("click", () => {
-    emailForm.classList.remove("focus-email");
     password.classList.add("focus-pass");
-})
+});
+
+password.addEventListener("blur", () => {
+    password.classList.remove("focus-pass");
+});
+
+loginButton.addEventListener("click", () => {
+    console.log('email:', emailForm.value);
+    console.log('password:', password.value) 
+    emailForm.value = '';
+    password.value = '';
+    if (checkbox.checked) {
+        console.log('Remind me:', true);
+        } else {
+            console.log('Remind me:', false);
+    }
+});
+
